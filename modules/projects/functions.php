b8c2bf120e382c7e46a69a4e22c784cd47e39daf
Slobodan Kustrimovic <skustrimovic@gmail.com>
2015-10-14 13:06:00 +0200

    Projects CPT and TAX args filters for developers

--- modules/projects/functions.php
+++ modules/projects/functions.php
@@ -22,11 +22,18 @@ $dslc_var_post_options['dslc-projects-post-options'] = array(
 	)
 );
 
+/**
+ * Register Post Type and Taxonomies
+ *
+ * @since 1.0
+ */
 function dslc_projects_module_cpt() {
 
+	// If module not active return
 	if ( ! dslc_is_module_active( 'DSLC_Projects', true ) )
 		return;
 
+	// Get capability
 	$capability = dslc_get_option( 'lc_min_capability_projects_m', 'dslc_plugin_options_access_control' );
 	if ( ! $capability ) $capability = 'publish_posts';
 
@@ -35,7 +42,12 @@ function dslc_projects_module_cpt() {
 	if ( empty ( $with_front )  ) $with_front = 'disabled';
 	if ( $with_front == 'enabled' ) $with_front = true; else $with_front = false;
 
-	register_post_type( 'dslc_projects', array(
+	/**
+	 * Register Post Type
+	 */
+
+	// Arguments
+	$cpt_args = array(
 		'menu_icon' => 'dashicons-feedback',
 		'labels' => array(
 			'name' => __( 'Projects', 'live-composer-page-builder' ),
@@ -66,38 +78,51 @@ function dslc_projects_module_cpt() {
 			'delete_post' => $capability,
 			'read_post' => $capability
 		),
-	));
-	
-	register_taxonomy(
-		'dslc_projects_cats', 
-		'dslc_projects', 
-		array( 
-			'labels' => array(
-				'name' => __( 'Projects Categories', 'live-composer-page-builder' ),
-				'singular_name' => __( 'Category', 'live-composer-page-builder' ),
-				'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
-				'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
-				'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
-				'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
-				'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
-				'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
-				'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
-				'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
-				'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
-			),
-			'hierarchical' => true, 
-			'public' => true, 
-			'rewrite' => array( 
-				'slug' => dslc_get_option( 'projects_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
-				'with_front' => $with_front
-			), 
-			'capabilities' => array(
-				'manage_terms' => $capability,
-				'edit_terms' => $capability,
-				'delete_terms' => $capability,
-				'assign_terms' => $capability,
-			),
-		)
 	);
+
+	// Apply filters
+	$cpt_args = apply_filters( 'dslc_projects_cpt_args', $cpt_args );
+
+	// Register Post Type
+	register_post_type( 'dslc_projects', $cpt_args );
+		
+	/**
+	 * Register Taxonomy ( Category )
+	 */
+
+	// Arguments
+	$tax_args = array( 
+		'labels' => array(
+			'name' => __( 'Projects Categories', 'live-composer-page-builder' ),
+			'singular_name' => __( 'Category', 'live-composer-page-builder' ),
+			'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
+			'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
+			'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
+			'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
+			'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
+			'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
+			'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
+			'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
+			'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
+		),
+		'hierarchical' => true, 
+		'public' => true, 
+		'rewrite' => array( 
+			'slug' => dslc_get_option( 'projects_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
+			'with_front' => $with_front
+		), 
+		'capabilities' => array(
+			'manage_terms' => $capability,
+			'edit_terms' => $capability,
+			'delete_terms' => $capability,
+			'assign_terms' => $capability,
+		),
+	);
+
+	// Apply filters
+	$tax_args = apply_filters( 'dslc_projects_cats_args', $tax_args );
+
+	// Register Taxonomy
+	register_taxonomy( 'dslc_projects_cats', 'dslc_projects', $tax_args ); 
 
 } add_action( 'init', 'dslc_projects_module_cpt' );
