e6d4de48d7a4e6063125056e8318e9f9267248e2
Slobodan Kustrimovic <skustrimovic@gmail.com>
2015-10-14 13:19:09 +0200

    Galleries CPT and TAX args filters for developers

--- modules/galleries/functions.php
+++ modules/galleries/functions.php
@@ -23,11 +23,18 @@ $dslc_var_post_options['dslc-gallery-post-options'] = array(
 	)
 );
 
+/**
+ * Register Post Type and Taxonomies
+ *
+ * @since 1.0
+ */
 function dslc_galleries_module_cpt() {
 
+	// If module not active return
 	if ( ! dslc_is_module_active( 'DSLC_Galleries', true ) )
 		return;
 
+	// Get capability
 	$capability = dslc_get_option( 'lc_min_capability_galleries_m', 'dslc_plugin_options_access_control' );
 	if ( ! $capability ) $capability = 'publish_posts';
 
@@ -36,7 +43,12 @@ function dslc_galleries_module_cpt() {
 	if ( empty ( $with_front )  ) $with_front = 'disabled';
 	if ( $with_front == 'enabled' ) $with_front = true; else $with_front = false;
 
-	register_post_type( 'dslc_galleries', array(
+	/**
+	 * Register Post Type
+	 */
+
+	// Arguments
+	$cpt_args = array(
 		'menu_icon' => 'dashicons-format-gallery',
 		'labels' => array(
 			'name' => __( 'Galleries', 'live-composer-page-builder' ),
@@ -67,32 +79,45 @@ function dslc_galleries_module_cpt() {
 			'delete_post' => $capability,
 			'read_post' => $capability
 		),
-	));
-	
-	register_taxonomy(
-		'dslc_galleries_cats', 
-		'dslc_galleries', 
-		array(
-			'labels' => array(
-				'name' => __( 'Galleries Categories', 'live-composer-page-builder' ),
-				'singular_name' => __( 'Category', 'live-composer-page-builder' ),
-				'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
-				'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
-				'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
-				'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
-				'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
-				'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
-				'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
-				'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
-				'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
-			),
-			'hierarchical' => true, 
-			'public' => true, 
-			'rewrite' => array( 
-				'slug' => dslc_get_option( 'galleries_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
-				'with_front' => $with_front
-			) 
-		)
 	);
+
+	// Apply filters
+	$cpt_args = apply_filters( 'dslc_galleries_cpt_args', $cpt_args );
+
+	// Register post type
+	register_post_type( 'dslc_galleries', $cpt_args );
+	
+	/**
+	 * Register Taxonomy ( Category )
+	 */
+
+	// Arguments
+	$cats_args = array(
+		'labels' => array(
+			'name' => __( 'Galleries Categories', 'live-composer-page-builder' ),
+			'singular_name' => __( 'Category', 'live-composer-page-builder' ),
+			'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
+			'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
+			'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
+			'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
+			'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
+			'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
+			'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
+			'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
+			'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
+		),
+		'hierarchical' => true, 
+		'public' => true, 
+		'rewrite' => array( 
+			'slug' => dslc_get_option( 'galleries_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
+			'with_front' => $with_front
+		) 
+	);
+
+	// Apply filters
+	$cats_args = apply_filters( 'dslc_galleries_cats_args', $cats_args );
+
+	// Register taxonomy
+	register_taxonomy( 'dslc_galleries_cats', 'dslc_galleries', $cats_args );
 
 } add_action( 'init', 'dslc_galleries_module_cpt' );
