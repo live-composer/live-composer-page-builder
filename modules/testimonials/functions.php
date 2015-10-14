a419bab02261c499f63d2ca42f8407348f82454e
Slobodan Kustrimovic <skustrimovic@gmail.com>
2015-10-14 13:33:50 +0200

    Testimonials CPT and TAX args filters for developers

--- modules/testimonials/functions.php
+++ modules/testimonials/functions.php
@@ -15,11 +15,18 @@ $dslc_var_post_options['dslc-testimonials-post-options'] = array(
 	)
 );
 
+/**
+ * Register Post Type and Taxonomies
+ *
+ * @since 1.0
+ */
 function dslc_testimonials_module_cpt() {
 
+	// If module not active return
 	if ( ! dslc_is_module_active( 'DSLC_Testimonials', true ) )
 		return;
 
+	// Get capability
 	$capability = dslc_get_option( 'lc_min_capability_testimonials_m', 'dslc_plugin_options_access_control' );
 	if ( ! $capability ) $capability = 'publish_posts';
 
@@ -28,7 +35,12 @@ function dslc_testimonials_module_cpt() {
 	if ( empty ( $with_front )  ) $with_front = 'disabled';
 	if ( $with_front == 'enabled' ) $with_front = true; else $with_front = false;
 
-	register_post_type( 'dslc_testimonials', array(
+	/**
+	 * Register Post Type
+	 */
+
+	// Arguments
+	$cpt_args = array(
 		'menu_icon' => 'dashicons-format-quote',
 		'labels' => array(
 			'name' => __( 'Testimonials', 'live-composer-page-builder' ),
@@ -59,39 +71,52 @@ function dslc_testimonials_module_cpt() {
 			'delete_post' => $capability,
 			'read_post' => $capability
 		),
-	));
-	
-	register_taxonomy(
-		'dslc_testimonials_cats', 
-		'dslc_testimonials', 
-		array( 
-			'labels' => array(
-				'name' => __( 'Testimonials Categories', 'live-composer-page-builder' ),
-				'singular_name' => __( 'Category', 'live-composer-page-builder' ),
-				'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
-				'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
-				'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
-				'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
-				'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
-				'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
-				'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
-				'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
-				'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
-			),
-			'hierarchical' => true, 
-			'public' => true,
-			'rewrite' => array( 
-				'slug' => dslc_get_option( 'testimonials_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
-				'with_front' => $with_front
-			),
-			'capabilities' => array(
-				'manage_terms' => $capability,
-				'edit_terms' => $capability,
-				'delete_terms' => $capability,
-				'assign_terms' => $capability,
-			),
-		)
 	);
+
+	// Apply filters
+	$cpt_args = apply_filters( 'dslc_testimonials_cpt_args', $cpt_args );
+
+	// Register post type
+	register_post_type( 'dslc_testimonials', $cpt_args );
+	
+	/**
+	 * Register Taxonomy ( Category )
+	 */
+
+	// Arguments
+	$cats_args = array( 
+		'labels' => array(
+			'name' => __( 'Testimonials Categories', 'live-composer-page-builder' ),
+			'singular_name' => __( 'Category', 'live-composer-page-builder' ),
+			'search_items'  => __( 'Search Categories', 'live-composer-page-builder' ),
+			'all_items' => __( 'All Categories', 'live-composer-page-builder' ),
+			'parent_item' => __( 'Parent Category', 'live-composer-page-builder' ),
+			'parent_item_colon' => __( 'Parent Category:', 'live-composer-page-builder' ),
+			'edit_item' => __( 'Edit Category', 'live-composer-page-builder' ),
+			'update_item' => __( 'Update Category', 'live-composer-page-builder' ),
+			'add_new_item' => __( 'Add New Category', 'live-composer-page-builder' ),
+			'new_item_name' => __( 'New Category Name', 'live-composer-page-builder' ),
+			'menu_name' => __( 'Categories', 'live-composer-page-builder' ),
+		),
+		'hierarchical' => true, 
+		'public' => true,
+		'rewrite' => array( 
+			'slug' => dslc_get_option( 'testimonials_cats_slug', 'dslc_plugin_options_cpt_slugs' ),
+			'with_front' => $with_front
+		),
+		'capabilities' => array(
+			'manage_terms' => $capability,
+			'edit_terms' => $capability,
+			'delete_terms' => $capability,
+			'assign_terms' => $capability,
+		),
+	);
+
+	// Apply filters
+	$cats_args = apply_filters( 'dslc_testimonials_cats_args', $cats_args );
+
+	// Register taxonomy
+	register_taxonomy( 'dslc_testimonials_cats', 'dslc_testimonials', $cats_args );
 
 } add_action( 'init', 'dslc_testimonials_module_cpt' );
 
