14672ed904757fcf3297035f08e03d750eaf5c89
Slobodan Kustrimovic <skustrimovic@gmail.com>
2015-11-18 20:37:01 +0100

    Remove Yoast SEO content filtering - Deprecated hook
    As of Yoast SEO 3.0 the wpseo_pre_analysis_post_content filter hook has
    been deprecated.

--- includes/other.php
+++ includes/other.php
@@ -3,29 +3,10 @@
 /**
  * Table of contents
  *
- * - dslc_yoast_seo_content_filter ( Include LC content in Yoast SEO filter )
  * - dslc_plugin_action_links ( Additional links on plugin listings page )
  * - dslc_icons
  */
 
-
-/**
- * Include LC content in Yoast SEO filter
- *
- * @since 1.0
- */
-
-function dslc_yoast_seo_content_filter( $post_content, $post ) {
-
-	// If there's LC content append it to the post content var
-	if ( get_post_meta( $post->ID, 'dslc_content_for_search', true ) ) {
-		$post_content .= ' ' . get_post_meta( $post->ID, 'dslc_content_for_search', true );
-	}
-
-	// Return the post content var
-	return $post_content;
-
-} add_filter( 'wpseo_pre_analysis_post_content', 'dslc_yoast_seo_content_filter', 10, 2 );
 
 /**
  * Additional links on plugin listings page 
