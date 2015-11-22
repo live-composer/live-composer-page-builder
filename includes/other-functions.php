3b45fc644ba974f2bf3d56cd901b42cf6b7f437d
Slobodan Kustrimovic <skustrimovic@gmail.com>
2015-11-22 04:16:21 +0100

    Image resize compatibility with Photon

--- includes/other-functions.php
+++ includes/other-functions.php
@@ -205,8 +205,30 @@ if ( ! function_exists('dslc_aq_resize') ) {
 	 *               Array contains 0 = URL, 1 = width, 2 = height
 	 */
 	function dslc_aq_resize( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
-		$aq_resize = DSLC_Aq_Resize::getInstance();
-		return $aq_resize->process( $url, $width, $height, $crop, $single, $upscale );
+
+		 if( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) {
+
+			$args = array(
+				'resize' => "$width,$height"
+			);
+			if ( $single == true ) {
+				return jetpack_photon_url( $url, $args );
+			} else {
+				$image = array (
+					0 => $img_url,
+					1 => $width,
+					2 => $height
+				);
+				return jetpack_photon_url( $url, $args );
+			}
+
+		} else {
+
+			$aq_resize = DSLC_Aq_Resize::getInstance();
+			return $aq_resize->process( $url, $width, $height, $crop, $single, $upscale );
+			
+		}
+
 	}
 
 }
