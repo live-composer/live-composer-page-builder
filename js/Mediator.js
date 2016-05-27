15389b4adb10489395c13313fe65588a47934370
ngorco <iwjkipas1>
2016-05-27 18:35:21 +0300

    LC2alpha: mediator fix

--- js/Mediator.js
+++ js/Mediator.js
@@ -146,6 +146,7 @@ var commonVars = DSLC.commonVars; /// Just alias
 			DSLC.ModulesManager.ActiveModules[moduleSettings.module_instance_id] = newModule;
 			jQuery( this ).after( moduleElem );
 			jQuery( this ).remove();
+			newModule.afterModuleRendered();
 
 			if( newModule.elem[0].innerText.match(/\[.*?\]/) && jQuery(".dslc-cached-version").find( "#dslc-module-" + newModule.settings.module_instance_id ).length > 0 )
 			{
