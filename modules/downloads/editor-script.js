b2d3795569f2bd55f0f0deb1e523f5d6d7963015
ngorco <iwjkipas1>
2016-05-29 02:01:21 +0300

    Lc2alpha: Projects, Staff, Galleries, Downloads, Partners - 40%
    Module ready for porting (files created, methods also) :
    - Projects
    - Staff
    - Galleries
    - Downloads
    - Partners

---
+++ modules/downloads/editor-script.js
@@ -0,0 +1,127 @@
+/**
+ * Downloads js extender
+ */
+
+'use strict'
+
+;(function(){
+
+	jQuery(document).on('DSLC_extend_modules', function(){
+
+		var Downloads = DSLC.ModulesManager.AvailModules.DSLC_Downloads;
+
+		Downloads.prototype.changeOptionsBeforeRender = function(options)
+		{
+			var opt = options;
+			/**
+			* Common
+			*/
+
+			opt.module_instance_id = this.settings.module_instance_id;
+			opt.posts_per_row.value = opt.posts_per_row.value ? opt.posts_per_row.value : opt.posts_per_row.std;
+			opt.columns_class = 'dslc-col dslc-' + 12 / opt.posts_per_row.value + '-col ';
+			opt.count = 0;
+			opt.real_count = 0;
+			opt.separator_enabled.value = opt.separator_enabled.value ? opt.separator_enabled.value : opt.separator_enabled.std;
+			opt.max_count = 12;
+			opt.type.value = opt.type.value ? opt.type.value : opt.type.std;
+
+			opt.main_location.value = opt.main_location.value ? opt.main_location.value : opt.main_location.std;
+
+			opt.meta_elements.value = opt.meta_elements.value ? opt.meta_elements.value : opt.meta_elements.std;
+			opt.elements.value = opt.elements.value ? opt.elements.value : opt.elements.std;
+
+			/**
+			* Header
+			*/
+			opt.show_filters = false;
+			opt.show_carousel_arrows = false;
+			opt.show_view_all_link = false;
+
+			opt.carousel_elements.value = opt.carousel_elements.value ?
+												opt.carousel_elements.value :
+			 									opt.carousel_elements.std;
+
+			if(opt.elements.value.indexOf('filters') > -1 && opt.type.value !== 'carousel'){
+
+				opt.show_filters = true;
+			}
+
+			if(opt.type.value == 'carousel' && 	opt.carousel_elements.value.indexOf('arrows') > -1){
+
+				opt.show_carousel_arrows = true;
+			}
+
+			/**
+			* Posts
+			*/
+			opt.post_elements.value = opt.post_elements.value ? opt.post_elements.value : opt.post_elements.std;
+
+			/**
+			* Dummy content
+			*/
+			opt.cats_array = [
+				{
+					cat_slug: 'troufle',
+					cat_name: 'Troufle'
+				},
+				{
+					cat_slug: 'icecream',
+					cat_name: 'Icecream'
+				},
+				{
+					cat_slug: 'cool_trees',
+					cat_name: 'Cool trees'
+				}
+			];
+
+			opt.posts = [];
+
+			var amount = opt.amount.value ? opt.amount.value : opt.amount.std;
+
+			for(var i = 0; i < amount; i++){
+
+				opt.posts.push({});
+			}
+
+			/**
+			 * Classes generation
+			 */
+
+			// Posts container
+			opt.container_class = 'dslc-posts dslc-cpt-posts dslc-clearfix dslc-cpt-posts-type-' +
+				opt.type.value + ' dslc-posts-orientation-' + opt.orientation.value + ' ';
+
+			if(opt.type.value == 'masonry'){
+
+				opt.container_class += 'dslc-init-masonry ';
+			}else if(opt.type.value == 'grid'){
+
+				opt.container_class += 'dslc-init-grid ';
+			}
+
+			// Post
+			opt.element_class = 'dslc-post dslc-cpt-post ';
+
+			if(opt.type.value == 'masonry'){
+
+				opt.element_class += 'dslc-masonry-item ';
+			}else if(opt.type.value == 'carousel'){
+
+				opt.element_class += 'dslc-carousel-item ';
+			}
+
+			opt.extra_class = 'in-cat-all';
+			opt.post_cats_data = {value: ''};
+			opt.manual_resize = false;
+
+			/**
+			 * Carousel Items
+			 */
+
+			opt.carousel_items = opt.posts_per_row.value ? 12 / opt.posts_per_row.value : 6;
+
+			return options;
+		}
+	});
+}());
--- modules/downloads/module.php
+++ modules/downloads/module.php
@@ -2052,6 +2052,482 @@ class DSLC_Downloads extends DSLC_Module {
 
 	}
 
+	/**
+	 * @inherited
+	 */
+	function afterRegister() {
+
+		add_action( 'wp_enqueue_scripts', function(){
+
+			global $LC_Registry;
+
+			$path = explode( '/', __DIR__ );
+			$path = array_pop( $path );
+
+			if ( $LC_Registry->get( 'dslc_active' ) == true ) {
+
+				wp_enqueue_script( 'js-downloads-editor-extender', DS_LIVE_COMPOSER_URL . '/modules/' . $path . '/editor-script.js', array( 'jquery' ) );
+			}
+
+			wp_enqueue_script( 'js-downloads-extender', DS_LIVE_COMPOSER_URL . '/modules/' . $path . '/script.js', array( 'jquery' ) );
+		});
+	}
+
+	/**
+	 * Returns filter HTML string
+	 * @return string
+	 */
+	function downloads_filter() {
+
+		global $LC_Registry;
+
+		$dslc_query = $this->get_posts();
+		$LC_Registry->set( 'dslc-downloads-query', $dslc_query );
+
+		$taxonomy_name = '';
+
+		$cats_array = array();
+
+		$cats_count = 0;
+
+		if ( $dslc_query->have_posts() ) {
+
+			while ( $dslc_query->have_posts() ) {
+
+				$dslc_query->the_post();
+
+				$cats_count++;
+
+				if ( $cats_count == 1 ) {
+
+					$post_type_taxonomies = get_object_taxonomies( get_post_type(), 'objects' );
+					foreach ( $post_type_taxonomies as $taxonomy ) {
+						if ( $taxonomy->hierarchical == true ) {
+							$taxonomy_name = $taxonomy->name;
+
+							$LC_Registry->set( 'dslc-downloads-taxonomy', $taxonomy_name );
+						}
+					}
+				}
+
+				$post_cats = get_the_terms( get_the_ID(), $taxonomy_name );
+				if ( ! empty( $post_cats ) ) {
+					foreach( $post_cats as $post_cat ) {
+						$cats_array[$post_cat->slug] = $post_cat->name;
+					}
+				}
+			}
+		}
+
+		ob_start();
+
+		foreach ( $cats_array as $cat_slug => $cat_name ) {?>
+			<span class="dslc-post-filter dslc-inactive" data-id="<?php echo $cat_slug; ?>"><?php echo $cat_name; ?></span>
+		<?php }
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Return categories data to each post. Template function.
+	 * @return string
+	 */
+	function downloads_categories() {
+
+		global $LC_Registry;
+
+		$taxonomy_name = $LC_Registry->get( 'dslc-downloads-taxonomy' );
+		$post_cats_data = '';
+
+		if ( isset( $taxonomy_name ) ) {
+			$post_cats = get_the_terms( get_the_ID(), $taxonomy_name );
+			if ( ! empty( $post_cats ) ) {
+				foreach( $post_cats as $post_cat ) {
+					$post_cats_data .= 'in-cat-' . $post_cat->slug . ' ';
+				}
+			}
+		}
+
+		return $post_cats_data . ' in-cat-all';
+	}
+
+	/**
+	 * Posts fetcher.
+	 * @return WP_Query
+	 */
+	function get_downloads() {
+
+		global $dslc_active;
+
+		$options = $this->getPropsValues();
+
+		// Fix slashes on apostrophes
+		if ( isset( $options['button_text'] ) ) {
+			$options['button_text'] = stripslashes( $options['button_text'] );
+		}
+
+		/* CUSTOM START */
+
+		if ( ! isset( $options['excerpt_length'] ) ) $options['excerpt_length'] = 20;
+
+		/**
+		 * Query
+		 */
+
+		// Fix for pagination
+		if( is_front_page() ) {
+
+			$paged = ( get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
+		} else {
+
+			$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+		}
+
+		// Fix for pagination from other modules affecting this one when pag disabled
+		if ( ! isset( $options['pagination_type'] ) || $options['pagination_type'] == 'disabled' ) {
+
+			$paged = 1;
+		}
+
+		// Fix for offset braking pagination
+		$query_offset = $options['offset'];
+
+		if ( $query_offset > 0 && $paged > 1 ) {
+
+			$query_offset = ( $paged - 1 ) * $options['amount'] + $options['offset'];
+		}
+
+		// General args
+		$args = array(
+			'paged' => $paged,
+			'post_type' => $options['post_type'],
+			'posts_per_page' => $options['amount'],
+			'order' => $options['order'],
+			'orderby' => $options['orderby'],
+		);
+
+		// Add offset
+		if ( $query_offset > 0 ) {
+			$args['offset'] = $query_offset;
+		}
+
+		if ( defined('DOING_AJAX') && DOING_AJAX ) {
+			$args['post_status'] = array( 'publish', 'private' );
+		}
+
+		// Category args
+		if ( isset( $options['categories'] ) && $options['categories'] != '' ) {
+			$cats_array = explode( ' ', $options['categories']);
+			$args['category__in'] = $cats_array;
+		}
+
+		// Exlcude and Include arrays
+		$exclude = array();
+		$include = array();
+
+		// Exclude current post
+		if ( is_singular( get_post_type() ) ) {
+
+			$exclude[] = get_the_ID();
+		}
+
+		// Exclude posts ( option )
+		if ( $options['query_post_not_in'] ) {
+
+			$exclude = array_merge( $exclude, explode( ' ', $options['query_post_not_in'] ) );
+		}
+
+		// Include posts ( option )
+ 		if ( $options['query_post_in'] ) {
+
+			$include = array_merge( $include, explode( ' ', $options['query_post_in'] ) );
+ 		}
+
+		// Include query parameter
+		if ( ! empty( $include ) ) {
+
+			$args['post__in'] = $include;
+		}
+
+		// Exclude query parameter
+		if ( ! empty( $exclude ) ) {
+
+			$args['post__not_in'] = $exclude;
+		}
+
+		// Author archive page
+		if ( is_author() && $options['query_alter'] == 'enabled' ) {
+
+			global $authordata;
+			$args['author__in'] = array( $authordata->data->ID );
+		}
+
+		// No paging
+		if ( $options['pagination_type'] == 'disabled' ) {
+
+			$args['no_found_rows'] = true;
+		}
+
+		// Sticky Posts
+		if ( $options['sticky_posts'] == 'disabled' ) {
+
+			$args['ignore_sticky_posts'] = true;
+		}
+
+		// Do the query
+		if ( ( is_category() || is_tag() || is_tax() || is_search() || is_date() ) && $options['query_alter'] == 'enabled' ) {
+
+			global $wp_query;
+			$dslc_query = $wp_query;
+		} else {
+
+			$dslc_query = new WP_Query( $args );
+		}
+
+		return $dslc_query;
+	}
+
+
+	/**
+	 * Posts render. Template function.
+	 *
+	 * @param  array $atts
+	 * @param  array $content
+	 *
+	 * @return string
+	 */
+	function render_downloads( $atts, $content ) {
+
+		global $LC_Registry;
+
+		$out = '';
+		$dslc_query = $LC_Registry->get( 'dslc-downloads-query' );
+
+		if ( $dslc_query == null ) {
+
+			$dslc_query = $this->get_downloads();
+			$LC_Registry->set( 'dslc-downloads-query', $dslc_query );
+		}
+
+		if ( $dslc_query->have_posts() ) {
+
+			$LC_Registry->set( 'curr_class', $this );
+
+			$options = $this->getPropsValues();
+			$cnt = 0;
+
+			while ( $dslc_query->have_posts() ) {
+
+				$dslc_query->the_post();
+				$LC_Registry->set( 'dslc-downloads-elem-index', $cnt );
+
+
+				$out .= DSLC_Main::dslc_do_shortcode( $content );
+
+				if ( 	$options['type'] == 'grid' &&
+				 		$cnt > 0 &&
+				 		($cnt + 1) % $options['posts_per_row'] == 0 &&
+				 		$options['separator_enabled'] != 'disabled' &&
+				 		($cnt + 1) < $dslc_query->found_posts &&
+				 		($cnt + 1) < $dslc_query->query_vars['posts_per_page']
+				 	) {
+
+					$out .= '<div class="dslc-post-separator"></div>';
+				}
+
+				$cnt++;
+			}
+
+			unset( $cnt );
+
+			$LC_Registry->set( 'dslc-downloads-elem-index', null );
+			$LC_Registry->set( 'curr_class', null );
+		}
+
+		return $out;
+	}
+
+	/**
+	 * Returns last col class
+	 * @return string
+	 */
+	function extra_col_class() {
+
+		global $LC_Registry;
+
+		$opts = $this->getPropsValues();
+		$index = $LC_Registry->get( 'dslc-downloads-elem-index' );
+		$extra_class = '';
+
+		if ( $opts['type'] == 'grid' && $index > 0 && ($index + 1) % $opts['posts_per_row'] == 0 && $opts['separator_enabled'] != 'disabled' ) {
+
+			$extra_class = 'dslc-last-col ';
+		}
+
+		if ( ! has_post_thumbnail() ) {
+
+			$extra_class .= 'dslc-post-no-thumb';
+		}
+
+		return $extra_class;
+	}
+
+	/**
+	 * Returns permalink. Repeater function.
+	 * @return  string
+	 */
+	function permalink() {
+
+		global $post;
+
+		return get_post_permalink( $post->ID );
+	}
+
+	/**
+	 * Returns excerpt or content. Repeater function.
+	 * @return string
+	 */
+	function excerpt() {
+
+		$options = $this->getPropsValues();
+
+		ob_start();
+		if ( $options['excerpt_or_content'] == 'content' ) {
+
+			the_content();
+		} else {
+
+			if ( $options['excerpt_length'] > 0 ) {
+				if ( has_excerpt() )
+					echo do_shortcode( wp_trim_words( get_the_excerpt(), $options['excerpt_length'] ) );
+				else
+					echo do_shortcode( wp_trim_words( get_the_content(), $options['excerpt_length'] ) );
+			} else {
+				if ( has_excerpt() )
+					echo do_shortcode( get_the_excerpt() );
+				else
+					echo do_shortcode( get_the_content() );
+			}
+		}
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Returns author's post link. Repeater function.
+	 * @return string
+	 */
+	function author_posts_link() {
+
+		ob_start();
+		echo get_the_author_posts_link();
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Returns post date. Repeater function.
+	 * @return  string
+	 */
+	function post_date() {
+
+		ob_start();
+		the_time( get_option( 'date_format' ) );
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Returns post title.Repeater function.
+	 * @return string
+	 */
+	function post_title() {
+
+		ob_start();
+		the_title();
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Returns post thumbnail. Repeater function.
+	 * @return  string
+	 */
+	function post_thumb() {
+
+		if ( ! has_post_thumbnail() ) return '';
+
+		$manual_resize = false;
+		$options = $this->getPropsValues();
+
+		if ( isset( $options['thumb_resize_height'] ) && ! empty( $options['thumb_resize_height'] ) || isset( $options['thumb_resize_width_manual'] ) && ! empty( $options['thumb_resize_width_manual'] ) ) {
+
+			$manual_resize = true;
+			$thumb_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+			$thumb_url = $thumb_url[0];
+
+			$thumb_alt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
+			if ( ! $thumb_alt ) $thumb_alt = '';
+
+			$resize_width = false;
+			$resize_height = false;
+
+			if ( isset( $options['thumb_resize_width_manual'] ) && ! empty( $options['thumb_resize_width_manual'] ) ) {
+				$resize_width = $options['thumb_resize_width_manual'];
+			}
+
+			if ( isset( $options['thumb_resize_height'] ) && ! empty( $options['thumb_resize_height'] ) ) {
+				$resize_height = $options['thumb_resize_height'];
+			}
+		}
+
+		ob_start();
+		?>
+		<div class="dslc-cpt-post-thumb-inner dslca-post-thumb">
+			<a href="<?php the_permalink() ?>">
+				<?php if ( $manual_resize ) {?>
+					<img src="<?php $res_img = dslc_aq_resize( $thumb_url, $resize_width, $resize_height, true ); echo $res_img; ?>" alt="<?php echo $thumb_alt; ?>" />
+				<?php } else { ?>
+					<?php the_post_thumbnail( 'full' ); ?>
+				<?php } ?>
+			</a>
+		</div><!-- .dslc-cpt-post-thumb-inner -->
+		<?php
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Returns navigation HTML. Template shortcode function
+	 * @param  array $atts
+	 * @return string
+	 */
+	function pagination_nav( $atts ) {
+
+		global $LC_Registry;
+
+		$options = $this->getPropsValues();
+		$dslc_query = $LC_Registry->get( 'dslc-downloads-query' );
+
+		ob_start();
+
+		if ( isset( $options['pagination_type'] ) && $options['pagination_type'] != 'disabled' ) {
+
+			$num_pages = $dslc_query->max_num_pages;
+
+			if ( $options['offset'] > 0 ) {
+				$num_pages = ceil ( ( $dslc_query->found_posts - $options['offset '] ) / $options['amount'] );
+			}
+
+			dslc_post_pagination( array( 'pages' => $num_pages, 'type' => $options['pagination_type'] ) );
+		}
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * @inherited
+	 */
 	function output( $options = [] ) {
 
 		$this->module_start();
