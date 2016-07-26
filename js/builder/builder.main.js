5be20aa4aa7c642ff878b1b0c2b6fd752e554780
Vlad Mitkovsky <vlad@lumbermandesigns.com>
2016-07-26 01:00:40 -0600

    – Reorganize all the JS and CSS files for automatic minimization

--- js/builder/builder.main.js
+++ js/builder/builder.main.js
@@ -12,6 +12,12 @@ var dslcAllFontsArray = dslcRegularFontsArray.concat( dslcGoogleFontsArray );
 var dslcIconsCurrentSet = DSLCIcons.fontawesome;
 var dslcDebug = true;
 
+
+// Global Plugin Object
+var DSLC = {
+	Production: {}
+};
+
 DSLC.Editor = {
 	flags: {
 		generate_code_after_row_changed: true
