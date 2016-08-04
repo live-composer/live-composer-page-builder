7c6d8a8543aaeb7871a613bc31fae93142223568
NGorco <iwjkipas1>
2016-08-04 23:22:27 +0300

    LC1.1[tinyMCE]: tinyMCE init from parent window
    - removed spare js file

--- js/builder.frontend/builder.frontend.main.js
+++ js/builder.frontend/builder.frontend.main.js
@@ -1,10 +1,4 @@
 /**
- * Iframe builder main scripts
- */
-
-var DSLC_Iframe = {};
-
-/**
  * Action - Disable links from going anywhere in editing mode.
  */
 jQuery(document).on( 'click', 'a:not(.dslca-link)', function(e){
