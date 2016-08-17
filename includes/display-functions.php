0f9527af1e2651cd2385003dcd11ca6eee5112e3
Vlad Mitkovsky <vlad@lumbermandesigns.com>
2016-08-16 20:24:46 -0600

    – Remove  Medium Inline Editor Styling – Update Sortable library – Hide admin top bar

--- includes/display-functions.php
+++ includes/display-functions.php
@@ -769,8 +769,11 @@ function dslc_filter_content( $content ) {
 			$composer_append .= '<input type="hidden" id="dslca-tut-page" value="' . get_the_ID() . '" />';
 		}
 
-		// Pass the filtered content output
-		return $composer_wrapper_before . do_action( 'dslc_output_prepend' ) . $composer_header . '<div id="dslc-main">' . $composer_prepend . $composer_content . '</div>' . $composer_append . $composer_footer . do_action( 'dslc_output_append' ) . $composer_wrapper_after . '<div class="lc-scroll-top-area"></div><div class="lc-scroll-bottom-area"></div>';
+		if ( dslc_is_editor_active( 'access' ) ) {
+			$composer_wrapper_after .= '<div class="lc-scroll-top-area"></div><div class="lc-scroll-bottom-area"></div>';
+		}
+		// Pass the filtered content output.
+		return $composer_wrapper_before . do_action( 'dslc_output_prepend' ) . $composer_header . '<div id="dslc-main">' . $composer_prepend . $composer_content . '</div>' . $composer_append . $composer_footer . do_action( 'dslc_output_append' ) . $composer_wrapper_after;
 
 	// If LC should not filter the content
 	} else {
