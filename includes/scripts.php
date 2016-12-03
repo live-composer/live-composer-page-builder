579c4a194a31d9d804e7d91ec5e2c8021890ee2c
lumberman <vlad@lumbermandesigns.com>
2016-12-03 13:52:58 -0700

    IE animations fix form #336

--- includes/scripts.php
+++ includes/scripts.php
@@ -51,6 +51,13 @@ final class DSLC_Scripts{
 		wp_enqueue_style( 'dslc-font-awesome', DS_LIVE_COMPOSER_URL . 'css/font-awesome.css', array(), DS_LIVE_COMPOSER_VER );
 
 		/**
+		 * Load our IE-only stylesheet for all versions of IE:
+		 * <!--[if IE]> ... <![endif]-->
+		 */
+		wp_enqueue_style( 'dslc-css-ie',  DS_LIVE_COMPOSER_URL . 'css/ie.css', array('dslc-main-css'), DS_LIVE_COMPOSER_VER );
+		wp_style_add_data( 'dslc-css-ie', 'conditional', 'IE' );
+
+		/**
 		 * JavaScript
 		 */
 		wp_enqueue_script( 'wp-mediaelement' ); // Used for playing videos.
