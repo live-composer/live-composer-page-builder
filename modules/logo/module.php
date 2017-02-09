3e1f158f7ee3249360d9c76aa1690f7cbea33f96
Dima <dima92.mvs@gmail.com>
2017-02-09 13:59:35 +0200

    Issue [#550](https://github.com/livecomposer/live-composer-page-builder/issues/550): Add native WP theme logo support
    #550

---
+++ modules/logo/module.php
@@ -0,0 +1,551 @@
+<?php
+/**
+ * Module Logo
+ *
+ * @package LiveComposer
+ */
+
+// Prevent direct access to the file.
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Module Class
+ */
+class DSLC_Logo extends DSLC_Module {
+
+	/**
+	 * Unique module id
+	 *
+	 * @var string
+	 */
+	var $module_id;
+
+	/**
+	 * Module label to show in the page builder
+	 *
+	 * @var string
+	 */
+	var $module_title;
+
+	/**
+	 * Module icon name (FontAwesome)
+	 *
+	 * @var string
+	 */
+	var $module_icon;
+
+	/**
+	 * Section in the modules panel that includes this module
+	 * Live Composer Extensions should use 'Extensions'
+	 *
+	 * @var string
+	 */
+	var $module_category;
+
+	/**
+	 * Construct
+	 */
+	function __construct() {
+
+		$this->module_id = 'DSLC_Logo';
+		$this->module_title = __( 'Logo', 'live-composer-page-builder' );
+		$this->module_icon = 'picture';
+		$this->module_category = 'general';
+
+	}
+
+	/**
+	 * Module options.
+	 * Function build array with all the module functionality and styling options.
+	 * Based on this array Live Composer builds module settings panel.
+	 * – Every array inside $dslc_options means one option = one control.
+	 * – Every option should have unique (for this module) id.
+	 * – Options divides on "Functionality" and "Styling".
+	 * – Styling options start with css_XXXXXXX
+	 * – Responsive options start with css_res_t_ (Tablet) or css_res_p_ (Phone)
+	 * – Options can be hidden.
+	 * – Options can have a default value.
+	 * – Options can request refresh from server on change or do live refresh via CSS.
+	 *
+	 * @return array All the module options in array.
+	 */
+	function options() {
+
+		$dslc_options = array(
+
+			array(
+				'label' => __( 'Show On', 'live-composer-page-builder' ),
+				'id' => 'css_show_on',
+				'std' => 'desktop tablet phone',
+				'type' => 'checkbox',
+				'choices' => array(
+					array(
+						'label' => __( 'Desktop', 'live-composer-page-builder' ),
+						'value' => 'desktop',
+					),
+					array(
+						'label' => __( 'Tablet', 'live-composer-page-builder' ),
+						'value' => 'tablet',
+					),
+					array(
+						'label' => __( 'Phone', 'live-composer-page-builder' ),
+						'value' => 'phone',
+					),
+				),
+			),
+
+			array(
+				'label' => __( 'Link - URL', 'live-composer-page-builder' ),
+				'id' => 'link_url',
+				'std' => '',
+				'type' => 'text',
+			),
+			array(
+				'label' => __( 'Link Type', 'live-composer-page-builder' ),
+				'id' => 'link_type',
+				'std' => 'none',
+				'type' => 'select',
+				'choices' => array(
+					array(
+						'label' => __( 'None', 'live-composer-page-builder' ),
+						'value' => 'none',
+					),
+					array(
+						'label' => __( 'URL - Same Tab', 'live-composer-page-builder' ),
+						'value' => 'url_same',
+					),
+					array(
+						'label' => __( 'URL - New Tab', 'live-composer-page-builder' ),
+						'value' => 'url_new',
+					),
+				),
+			),
+			array(
+				'label' => __( 'Resize - Height', 'live-composer-page-builder' ),
+				'id' => 'resize_height',
+				'std' => '',
+				'type' => 'text',
+			),
+			array(
+				'label' => __( 'Resize - Width', 'live-composer-page-builder' ),
+				'id' => 'resize_width',
+				'std' => '',
+				'type' => 'text',
+			),
+			array(
+				'label' => __( 'Logo - TITLE attribute', 'live-composer-page-builder' ),
+				'id' => 'logo_title',
+				'std' => '',
+				'type' => 'text',
+			),
+			array(
+				'label' => __( 'Logo - ALT attribute', 'live-composer-page-builder' ),
+				'id' => 'logo_alt',
+				'std' => '',
+				'type' => 'text',
+			),
+
+			/**
+			 * Styling
+			 */
+
+			array(
+				'label' => __( 'Align', 'live-composer-page-builder' ),
+				'id' => 'css_align',
+				'std' => '',
+				'type' => 'text_align',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'text-align',
+				'section' => 'styling',
+			),
+			array(
+				'label' => __( 'BG Color', 'live-composer-page-builder' ),
+				'id' => 'css_bg_color',
+				'std' => '',
+				'type' => 'color',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'background-color',
+				'section' => 'styling',
+			),
+			array(
+				'label' => __( 'Border Color', 'live-composer-page-builder' ),
+				'id' => 'css_border_color',
+				'std' => '',
+				'type' => 'color',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'border-color',
+				'section' => 'styling',
+			),
+			array(
+				'label' => __( 'Border Width', 'live-composer-page-builder' ),
+				'id' => 'css_border_width',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'border-width',
+				'section' => 'styling',
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Borders', 'live-composer-page-builder' ),
+				'id' => 'css_border_trbl',
+				'std' => 'top right bottom left',
+				'type' => 'checkbox',
+				'choices' => array(
+					array(
+						'label' => __( 'Top', 'live-composer-page-builder' ),
+						'value' => 'top',
+					),
+					array(
+						'label' => __( 'Right', 'live-composer-page-builder' ),
+						'value' => 'right',
+					),
+					array(
+						'label' => __( 'Bottom', 'live-composer-page-builder' ),
+						'value' => 'bottom',
+					),
+					array(
+						'label' => __( 'Left', 'live-composer-page-builder' ),
+						'value' => 'left',
+					),
+				),
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'border-style',
+				'section' => 'styling',
+			),
+			array(
+				'label' => __( 'Border Radius', 'live-composer-page-builder' ),
+				'id' => 'css_border_radius',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'border-radius',
+				'section' => 'styling',
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Margin Bottom', 'live-composer-page-builder' ),
+				'id' => 'css_margin_bottom',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'margin-bottom',
+				'section' => 'styling',
+				'ext' => 'px',
+				'min' => -150,
+				'max' => 150,
+			),
+			array(
+				'label' => __( 'Padding Vertical', 'live-composer-page-builder' ),
+				'id' => 'css_padding_vertical',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-top,padding-bottom',
+				'section' => 'styling',
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Padding Horizontal', 'live-composer-page-builder' ),
+				'id' => 'css_padding_horizontal',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-left,padding-right',
+				'section' => 'styling',
+				'ext' => 'px',
+			),
+
+			/**
+			 * Image
+			 */
+
+			array(
+				'label' => __( 'Border Color', 'live-composer-page-builder' ),
+				'id' => 'css_image_border_color',
+				'std' => '',
+				'type' => 'color',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo img',
+				'affect_on_change_rule' => 'border-color',
+				'section' => 'styling',
+				'tab' => __( 'Image', 'live-composer-page-builder' ),
+			),
+			array(
+				'label' => __( 'Border Width', 'live-composer-page-builder' ),
+				'id' => 'css_image_border_width',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo img',
+				'affect_on_change_rule' => 'border-width',
+				'section' => 'styling',
+				'tab' => __( 'Image', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Borders', 'live-composer-page-builder' ),
+				'id' => 'css_image_border_trbl',
+				'std' => 'top right bottom left',
+				'type' => 'checkbox',
+				'choices' => array(
+					array(
+						'label' => __( 'Top', 'live-composer-page-builder' ),
+						'value' => 'top',
+					),
+					array(
+						'label' => __( 'Right', 'live-composer-page-builder' ),
+						'value' => 'right',
+					),
+					array(
+						'label' => __( 'Bottom', 'live-composer-page-builder' ),
+						'value' => 'bottom',
+					),
+					array(
+						'label' => __( 'Left', 'live-composer-page-builder' ),
+						'value' => 'left',
+					),
+				),
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo img',
+				'affect_on_change_rule' => 'border-style',
+				'section' => 'styling',
+				'tab' => __( 'Image', 'live-composer-page-builder' ),
+			),
+			array(
+				'label' => __( 'Border Radius', 'live-composer-page-builder' ),
+				'id' => 'css_image_border_radius',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo img',
+				'affect_on_change_rule' => 'border-radius',
+				'section' => 'styling',
+				'tab' => __( 'Image', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+
+			/**
+			 * Responsive Tablet
+			 */
+
+			array(
+				'label' => __( 'Responsive Styling', 'live-composer-page-builder' ),
+				'id' => 'css_res_t',
+				'std' => 'disabled',
+				'type' => 'select',
+				'choices' => array(
+					array(
+						'label' => __( 'Disabled', 'live-composer-page-builder' ),
+						'value' => 'disabled',
+					),
+					array(
+						'label' => __( 'Enabled', 'live-composer-page-builder' ),
+						'value' => 'enabled',
+					),
+				),
+				'section' => 'responsive',
+				'tab' => __( 'tablet', 'live-composer-page-builder' ),
+			),
+			array(
+				'label' => __( 'Margin Bottom', 'live-composer-page-builder' ),
+				'id' => 'css_res_t_margin_bottom',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'margin-bottom',
+				'section' => 'responsive',
+				'tab' => __( 'tablet', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Padding Vertical', 'live-composer-page-builder' ),
+				'id' => 'css_res_t_padding_vertical',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-top,padding-bottom',
+				'section' => 'responsive',
+				'tab' => __( 'tablet', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Padding Horizontal', 'live-composer-page-builder' ),
+				'id' => 'css_res_t_padding_horizontal',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-left,padding-right',
+				'section' => 'responsive',
+				'tab' => __( 'tablet', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+
+			/**
+			 * Responsive Phone
+			 */
+
+			array(
+				'label' => __( 'Responsive Styling', 'live-composer-page-builder' ),
+				'id' => 'css_res_p',
+				'std' => 'disabled',
+				'type' => 'select',
+				'choices' => array(
+					array(
+						'label' => __( 'Disabled', 'live-composer-page-builder' ),
+						'value' => 'disabled',
+					),
+					array(
+						'label' => __( 'Enabled', 'live-composer-page-builder' ),
+						'value' => 'enabled',
+					),
+				),
+				'section' => 'responsive',
+				'tab' => __( 'phone', 'live-composer-page-builder' ),
+			),
+			array(
+				'label' => __( 'Margin Bottom', 'live-composer-page-builder' ),
+				'id' => 'css_res_p_margin_bottom',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'margin-bottom',
+				'section' => 'responsive',
+				'tab' => __( 'phone', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Padding Vertical', 'live-composer-page-builder' ),
+				'id' => 'css_res_p_padding_vertical',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-top,padding-bottom',
+				'section' => 'responsive',
+				'tab' => __( 'phone', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+			array(
+				'label' => __( 'Padding Horizontal', 'live-composer-page-builder' ),
+				'id' => 'css_res_p_padding_horizontal',
+				'std' => '0',
+				'type' => 'slider',
+				'refresh_on_change' => false,
+				'affect_on_change_el' => '.dslc-logo',
+				'affect_on_change_rule' => 'padding-left,padding-right',
+				'section' => 'responsive',
+				'tab' => __( 'phone', 'live-composer-page-builder' ),
+				'ext' => 'px',
+			),
+
+		);
+
+		$dslc_options = array_merge( $dslc_options, $this->shared_options( 'animation_options', array( 'hover_opts' => false ) ) );
+		$dslc_options = array_merge( $dslc_options, $this->presets_options() );
+
+		return apply_filters( 'dslc_module_options', $dslc_options, $this->module_id );
+	}
+
+	/**
+	 * Output the module render
+	 *
+	 * @param  array $options All the plugin options.
+	 * @return void
+	 */
+	function output( $options ) {
+
+		$this->module_start( $options );
+
+		global $dslc_active;
+
+		if ( $dslc_active && is_user_logged_in() && current_user_can( DS_LIVE_COMPOSER_CAPABILITY ) ) {
+			$dslc_is_admin = true;
+		} else {
+			$dslc_is_admin = false;
+		}
+
+		$anchor_class = '';
+		$anchor_target = '_self';
+		$anchor_href = '#';
+
+		if ( 'url_new' === $options['link_type'] ) {
+			$anchor_target = '_blank';
+		}
+
+		if ( '' !== $options['link_url'] ) {
+			$anchor_href = do_shortcode( $options['link_url'] );
+		}
+
+		?>
+
+		<div class="dslc-logo">
+
+			<?php if ( ! has_custom_logo() ) : ?>
+
+				<div class="dslc-notification dslc-red"><?php _e( 'No logo has been set yet. Please add your logo here: WP Admin -> Appearance -> Customize -> Site Identity -> Logo.', 'live-composer-page-builder' ); ?></div>
+
+			<?php else : ?>
+
+				<?php
+
+				$site_logo_id = get_theme_mod( 'custom_logo' );
+				$site_logo = wp_get_attachment_image_src( $site_logo_id, 'full' );
+				$logo_url = $site_logo['0'];
+
+				$resize = false;
+				$the_image = $logo_url;
+
+				if ( '' != $options['resize_width'] || '' != $options['resize_height'] ) {
+
+					$resize = true;
+					$resize_width = false;
+					$resize_height = false;
+
+					if ( '' != $options['resize_width'] ) {
+						$resize_width = $options['resize_width'];
+					}
+
+					if ( '' != $options['resize_height'] ) {
+						$resize_height = $options['resize_height'];
+					}
+
+					$the_image = dslc_aq_resize( $logo_url, $resize_width, $resize_height, true );
+				}
+
+				?>
+
+				<?php if ( 'none' !== $options['link_type'] ) : ?>
+					<a href="<?php echo $anchor_href; ?>" target="<?php echo $anchor_target; ?>">
+				<?php endif; ?>
+					<img src="<?php echo $the_image ?>" alt="<?php echo $options['logo_alt']; ?>" title="<?php echo $options['logo_title']; ?>" />
+				<?php if ( 'none' !== $options['link_type'] ) : ?>
+					</a>
+				<?php endif; ?>
+
+			<?php endif; ?>
+
+		</div>
+
+		<?php
+
+		$this->module_end( $options );
+
+	}
+}
