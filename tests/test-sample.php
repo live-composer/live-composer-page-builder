bb95dbeb28ad080db3a3b50e34fb5240692f65f0
lumberman <vlad@lumbermandesigns.com>
2017-11-08 19:32:47 -0700

    – Ready to use unit tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,28 +1,38 @@
 <?php
+/**
+ * Class SampleTest
+ *
+ * @package Live_Composer_Page_Builder
+ */
 
+/**
+ * Sample test case.
+ */
 class SampleTest extends WP_UnitTestCase {
 
-	function setUp() {
+	/**
+	 * Test some important constants
+	 */
+	public function test_constants() {
+ 
+		// $stats = new EDD_Payment_Stats;
+		// $sales = $stats->get_sales( 0, 'this_month' );
 
-		
-	}
+		// $this->assertEquals( 1, $sales );
+		// $this->assertGreaterThan( 0, $sales );
+		// $this->assertLessThan( 10, $sales );
 
-	function tearDown() {
-
-	}
-
-	function test_dslc_get_new_module_id() {
-
-		$new_id = dslc_get_new_module_id();
-		$this->assertNull( $new_id, 'User not logged in.' );
-
-		$current_user = new WP_User( 1 );
-		$current_user->set_role( 'administrator' );
-
-		wp_set_current_user( 1 );
-		$new_id = dslc_get_new_module_id();
-		$this->assertInternalType( 'int', 'fgfg', 'User logged in as administrator.' );
-		// replace this with some actual testing code
-		//$this->assertTrue( class_exists( 'DSLC_Module' ) );
+		// Extract version number from the plugin meta comment block.
+		$path_to_current_folder = __DIR__;
+		$abspath = str_replace( '/tests', '', $path_to_current_folder );
+		$version = get_file_data( $abspath . '/ds-live-composer.php', array( 'Version' ), 'plugin' );
+		$version = $version[0];
+		// Compare DS_LIVE_COMPOSER_VER with extracted above version.
+		$this->assertEquals( DS_LIVE_COMPOSER_VER, $version );
+		$this->assertFalse( DS_LIVE_COMPOSER_DEV_MODE );
+		$this->assertFalse( DS_LIVE_COMPOSER_ACTIVE );
+		$this->assertEquals( DS_LIVE_COMPOSER_SHORTNAME, 'Live Composer' );
+		// $this->assertEquals( DS_LIVE_COMPOSER_BASENAME, 'live-composer-page-builder' );
+	
 	}
 }
