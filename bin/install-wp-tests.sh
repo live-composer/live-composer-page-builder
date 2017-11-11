9dd20ff1d73f3a62706f0692d210b5f5274a9238
lumberman <vlad@lumbermandesigns.com>
2017-11-10 22:51:18 -0700

    Another try.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -156,46 +156,8 @@ install_db() {
 	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
-wpcept-run() {
-
-	# if [[ $DO_WP_CEPT == 0 ]]; then
-	# 	echo Not running codecept tests.
-	# 	return
-	# fi
-
-	WP_CEPT_SERVER='127.0.0.1:8080'
-	# Custom Code: Start server for codeception tests.
-	# We start the server up early so that it has time to prepare.
-	php -S "$WP_CEPT_SERVER" -t "$WP_CORE_DIR" &
-
-	# export WP_DEVELOP_DIR=/tmp/wordpress
-
-	# Configure WordPress for access through a web server.
-	cd "$WP_CORE_DIR"
-	sed -i "s/example.org/$WP_CEPT_SERVER/" wp-tests-config.php
-	cp wp-tests-config.php wp-config.php
-	echo "
-		if ( ! defined( 'WP_INSTALLING' ) && ( getenv( 'WP_MULTISITE' ) || file_exists( dirname( __FILE__ ) . '/is-multisite' ) ) ) {
-			define( 'MULTISITE', true );
-			define( 'SUBDOMAIN_INSTALL', false );
-			\$GLOBALS['base'] = '/';
-		}
-		require_once(ABSPATH . 'wp-settings.php');
-	" >> wp-config.php
-
-	cd -
-
-	# sed -i "s/wptests.local/$WP_CEPT_SERVER/" codeception.dist.yml
-	# phantomjs --webdriver=4444 >/dev/null &
-
-	# Give PhantomJS time to start.
-	# sleep 3
-
-	php vendor/bin/codecept run acceptance --steps --debug -vvv --env travis
-}
-
 install_wp
 install_test_suite
 install_db
 
-wpcept-run # custom
+# wpcept-run # custom
