f54a9708afe7a9d9682a6e5f359cbbd7ae9aa2c9
lumberman <vlad@lumbermandesigns.com>
2017-11-14 10:55:29 -0700

    fix a wrong path in php unit config

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -22,7 +22,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/ds-live-composer.php';
+	require dirname( dirname( __FILE__ ) ) . '../../ds-live-composer.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
