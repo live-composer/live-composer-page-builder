77afe3867d120df8087717b96e3f0365a05800cc
lumberman <vlad@lumbermandesigns.com>
2017-12-12 00:17:06 -0700

    Debugging.

--- js/builder/builder.uigeneral.functions.js
+++ js/builder/builder.uigeneral.functions.js
@@ -62,35 +62,32 @@ jQuery(document).ready(function($) {
  	jQuery('body').addClass('dslca-enabled dslca-drag-not-in-progress');
  	jQuery('.dslca-invisible-overlay').hide();
  	jQuery('.dslca-section').eq(0).show();
-
-
-	/** Wait till tinyMCE loaded */
-	window.previewAreaTinyMCELoaded = function(){
-
-		var self = this;
-		LiveComposer.Builder.PreviewAreaWindow = this;
-		LiveComposer.Builder.PreviewAreaDocument = jQuery(this.document);
-
-		// Disable WP admin bar in editing mode
-		jQuery('#wpadminbar', LiveComposer.Builder.PreviewAreaDocument).remove();
-
-		LiveComposer.Builder.UI.initInlineEditors();
-		dslc_fix_contenteditable();
-
-		var mainDraggable = LiveComposer.Builder.PreviewAreaDocument.find("#dslc-main").eq(0)[0];
-		new LiveComposer.Builder.Elements.CSectionsContainer( mainDraggable );
-
-		jQuery(document).trigger('editorFrameLoaded');
-
-		dslc_drag_and_drop();
-
-		dslc_generate_code();
-
-		// Catch keypress events (from both parent and iframe) to add keyboard support
-		dslc_keypress_events();
-		LiveComposer.Builder.UI.initPreviewAreaScroller();
-	};
 });
+
+/** Wait till tinyMCE loaded */
+window.previewAreaTinyMCELoaded = function(){
+
+	var self = this;
+	LiveComposer.Builder.PreviewAreaWindow = this;
+	LiveComposer.Builder.PreviewAreaDocument = jQuery(this.document);
+
+	// Disable WP admin bar in editing mode
+	jQuery('#wpadminbar', LiveComposer.Builder.PreviewAreaDocument).remove();
+
+	LiveComposer.Builder.UI.initInlineEditors();
+	dslc_fix_contenteditable();
+
+	var mainDraggable = LiveComposer.Builder.PreviewAreaDocument.find("#dslc-main").eq(0)[0];
+	new LiveComposer.Builder.Elements.CSectionsContainer( mainDraggable );
+
+	jQuery(document).trigger('editorFrameLoaded');
+	dslc_drag_and_drop();
+	dslc_generate_code();
+
+	// Catch keypress events (from both parent and iframe) to add keyboard support
+	dslc_keypress_events();
+	LiveComposer.Builder.UI.initPreviewAreaScroller();
+};
 
 /**
  * Action - "Currently Editing" scroll on click
