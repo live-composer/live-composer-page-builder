dc14f1879c445e8d6c7a779c930c1da9f329e68d
shuvo247 <mdshakibul.shuvo@gmail.com>
2022-08-10 19:54:40 +0600

    PHP Deprecated: Automatic conversion of false issue finally solved

--- includes/class-dslc-cache.php
+++ includes/class-dslc-cache.php
@@ -183,7 +183,9 @@ class DSLC_Cache {
 	public function set_cache( $code_to_cache = false, $identificator = false, $cache_type = false ) {
 		if ( self::$enabled && $code_to_cache ) {
 
-			if ( !isset( $cache_type ) ) {
+			if(!is_array(self::$cache)) self::$cache = [];
+
+			if ( ! $cache_type ) {
 				$cache_type = $this->type;
 			}
 
